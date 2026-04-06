5cca6e1a8bd0a61feb3ab09e5cc113d8136e2d9c
Sunnycidal <manow.lucky@gmail.com>
2026-04-06 10:29:48 -0300

    acabou

--- src/main/java/programas/prova/Doideira.java
+++ src/main/java/programas/prova/Doideira.java
@@ -22,7 +22,7 @@ public class Doideira {
                 """);
 
         String carrinho;
-        double $1 = 320.00, $2 = 180.00, $3 = 450.00, $4 = 270.00, $5 = 390.00,
+        double $1 = 10000.00, $2 = 180.00, $3 = 450.00, $4 = 270.00, $5 = 390.00,
                 subTotal = 0;
 
         System.out.println("Escolha o código de um produto!");
@@ -32,7 +32,7 @@ public class Doideira {
         if(carrinho.matches("^201$"))
         {
             System.out.println("TECLADO");
-            System.out.println("Informe a quantidade(Máx 10): ");
+            System.out.println("Informe a quantidade(Máx 20): ");
             if (sc.hasNextInt()){
                 int quantidade = sc.nextInt();
                 if(quantidade >0 && quantidade <21){subTotal = quantidade * $1;}
@@ -47,7 +47,7 @@ public class Doideira {
         else if(carrinho.matches("^202$"))
         {
             System.out.println("MOUSE GAMER");
-            System.out.println("Informe a quantidade(Máx 10): ");
+            System.out.println("Informe a quantidade(Máx 20): ");
             if (sc.hasNextInt()){
                 int quantidade = sc.nextInt();
                 if(quantidade >0 && quantidade <21){subTotal = quantidade * $2;}
@@ -62,7 +62,7 @@ public class Doideira {
         else if(carrinho.matches("^203$"))
         {
             System.out.println("HD EXTERNO 1TB");
-            System.out.println("Informe a quantidade(Máx 10): ");
+            System.out.println("Informe a quantidade(Máx 20): ");
             if (sc.hasNextInt()){
                 int quantidade = sc.nextInt();
                 if(quantidade >0 && quantidade <21){subTotal = quantidade * $3;}
@@ -77,7 +77,7 @@ public class Doideira {
         else if(carrinho.matches("^204$"))
         {
             System.out.println("ROTEADOR WIRELESS");
-            System.out.println("Informe a quantidade(Máx 10): ");
+            System.out.println("Informe a quantidade(Máx 20): ");
             if (sc.hasNextInt()){
                 int quantidade = sc.nextInt();
                 if(quantidade >0 && quantidade <21){subTotal = quantidade * $4;}
@@ -92,7 +92,7 @@ public class Doideira {
         else if(carrinho.matches("^205$"))
         {
             System.out.println("WEBCAM FULL HD");
-            System.out.println("Informe a quantidade(Máx 10): ");
+            System.out.println("Informe a quantidade(Máx 20): ");
             if (sc.hasNextInt()){
                 int quantidade = sc.nextInt();
                 if(quantidade >0 && quantidade <21){subTotal = quantidade * $5;}
@@ -109,27 +109,6 @@ public class Doideira {
         }
 
 
-        System.out.println("""
-                SERVIÇOS ADICIONAIS
-                -------------------
-                SEGURO DE CARGA.................+6%
-                RASTREAMENTO EM TEMPO REAL......+4%
-                ESCOLTA ARMADA.................+12%
-                """);
-
-        double adicional = 0;
-
-        System.out.println("Deseja acrescentar o seguro? (S/N)");
-        String respostaSeguro = sc.next().toUpperCase();
-        if (respostaSeguro.matches("^S$")){adicional += subTotal * 0.06;}
-
-        System.out.println("Deseja acrescentar o Rastreamento? (S/N)");
-        String respostaRastreamento = sc.next().toUpperCase();
-        if (respostaRastreamento.matches("^S$")){adicional += subTotal * 0.04;}
-
-        System.out.println("Deseja acrescentar a escolta armada? (S/N)");
-        String respostaEscolta = sc.next().toUpperCase();
-        if (respostaEscolta.matches("^S$")){adicional += subTotal * 0.12;}
 
 
         System.out.println("Informe sua região\n");
@@ -143,6 +122,7 @@ public class Doideira {
 
         String regiao = sc.next().toUpperCase();
         double adicionalRegional = 0;
+        double subTotalAntigo = subTotal;
 
         if (regiao.matches("^1$")){adicionalRegional = subTotal * 0.06;}
         else if (regiao.matches("^2$")){adicionalRegional = subTotal * 0.04;}
@@ -153,6 +133,44 @@ public class Doideira {
             System.out.println("Opção inválida!");
             return;
         }
+
+
+        System.out.println("""
+                SERVIÇOS ADICIONAIS
+                -------------------
+                SEGURO DE CARGA.................+6%
+                RASTREAMENTO EM TEMPO REAL......+4%
+                ESCOLTA ARMADA.................+12%
+                """);
+
+        double adicional = 0;
+
+        System.out.println("Deseja acrescentar o seguro? (S/N)");
+        String respostaSeguro = sc.next().toUpperCase();
+        if (respostaSeguro.matches("^S$")){adicional += subTotal * 0.06;
+            System.out.println("Adiconamos o seguro!");}
+        else{System.out.println("Seguro não adicionado");}
+
+        System.out.println("Deseja acrescentar o Rastreamento? (S/N)");
+        String respostaRastreamento = sc.next().toUpperCase();
+        if (respostaRastreamento.matches("^S$")){adicional += subTotal * 0.04;
+            System.out.println("Adicionamos o rastreamento");}
+        else{System.out.println("Rastreamento não adicionado");}
+
+
+        System.out.println("Deseja acrescentar a escolta armada? (S/N)");
+        String respostaEscolta = sc.next().toUpperCase();
+
+        if (!(regiao.equals("1") || regiao.equals("2"))){
+
+            if (respostaEscolta.matches("^S$") ){adicional += subTotal * 0.12;
+                System.out.println("Adicionamos a escolta armada!");}
+            else{System.out.println("Escolta não adicionada");}
+        }else{
+            System.out.println("Escolta não disponível para as suas regiões");
+        }
+
+
 
         System.out.println("""
                 1. Caixa simples.........R$12,00
@@ -184,15 +202,15 @@ public class Doideira {
 
         double valorFinal = subTotal+adicionalRegional+adicionalEmbalagem+adicional;
 
-        System.out.println("\n--- RESUMO DO PEDIDO ---");
-        System.out.printf("Subtotal:              R$ %10.2f%n", subTotal);
+        System.out.println("\n--------- RESUMO DO PEDIDO ---------");
+        System.out.printf("Subtotal:              R$ %10.2f%n", subTotalAntigo);
         System.out.printf("Custo Frete:           R$ %10.2f%n", adicionalRegional);
         System.out.printf("Custo Embalagem:       R$ %10.2f%n", adicionalEmbalagem);
         System.out.printf("Serviços Adicionais:   R$ %10.2f%n", adicional);
         if (valorDesconto>0) {
             System.out.printf("Valor do desconto:     R$ %10.2f%n", valorDesconto);
         }
-        System.out.println("----------------------------------");
+        System.out.println("------------------------------------");
         System.out.printf("VALOR TOTAL:           R$ %10.2f%n", valorFinal);
 
 
